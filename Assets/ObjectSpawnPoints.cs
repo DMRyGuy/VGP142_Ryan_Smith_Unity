1265fe9e51cee9096a34de1ffab724b58aa4c9f7
DMRyGuy <dmryansmith@gmail.com>
2022-07-22 13:21:03 -0400

    Collectible spawn points initialized
    Spawn points for collectibles and the randomization complete, also corrected the orientation of the collectibles to ensure they appear properly when spawned.

---
+++ Assets/ObjectSpawnPoints.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawnPoints : MonoBehaviour
+{
+
+    public GameObject[] collectiblePrefabArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+            Instantiate(collectiblePrefabArray[Random.Range(0, 4)], transform.position, transform.rotation);
+            throw new UnassignedReferenceException(" Collectible not appearing " + name + " back off get your own sandwich. ");
+        }
+
+        finally
+        {
+            Debug.Log("Objects are placed. You can continue.");
+        }
+    }
+}
